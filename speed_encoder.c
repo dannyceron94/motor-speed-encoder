7630916a3096eeac4a04a59384fb083b7cad87e8
dannyceron94 <dannyceron94@gmail.com>
2020-04-20 14:52:35 -0700

    ...

--- speed_encoder.c
+++ speed_encoder.c
@@ -57,7 +57,7 @@ int activate(int rot){
             tempCount = tempCount-1;
         }
         exitCount++;
-        if(exitCount>100){
+        if(exitCount>10000000){
             break;
         }
        
