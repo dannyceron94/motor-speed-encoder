098bf5a15bd1a8ef961f67f03c1daf1aae5e1990
dannyceron94 <dannyceron94@gmail.com>
2020-04-20 18:56:03 -0700

    ...

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main() {
    // run(4);
    int m1 = init("motor1","config2");
    int m2 = init("motor2","config1");
-   allForward(15);
+   allForward(26);
   
    activate(1000);
    sleep(1);
