f4a61a0941951d72841a9445297ea6bc8a7859eb
dannyceron94 <dannyceron94@gmail.com>
2020-04-23 19:29:26 -0700

    ...

--- speed_encoder.c
+++ speed_encoder.c
@@ -117,7 +117,7 @@ int active_LSI(){
     init_LSI(12,13,14);
     printf("%d\n",wiringPiSPIDataRW (CHANNEL,send,4));
     printf("hex%x\n",send);
-    printf("%s",send[1]);
+    printf("%n",send[0]);
     
     return 0;
 }
